1b32ed67993c0a6179774b3149f7fcc1ec14e070
Nikhil Sharma <79254517+nikhilsharma1574@users.noreply.github.com>
2021-07-13 21:52:14 +0530

    Update index.c

--- index.c
+++ index.c
@@ -12,7 +12,12 @@ int main()
  time_t t; // Time k liye
  time(&t);
    int c,date,year,adult,child,infant,age;
-   char o[100],d[100],month[50],meal[10],first_name[50];
+   char o[100],d[100],month[50],meal[10];
+   struct passanger
+   {
+     char name[50];
+     int age;
+   };
     gotoxy(37,2);
      printf("Welcome To\n");
     gotoxy(33,4);
@@ -29,47 +34,59 @@ int main()
      scanf("%d",&c);
       if(c==1)
        {
-         clrscr();
-         gotoxy(22,2);
-         printf("SRM Welcomes You to Flight Booking Gateway\n\n");
-         printf("--------------------------------------------------------------------------------\n");
-         f=fopen("flight.txt","a");
-         fprintf(f,"\n");
-         printf("Enter Origin = ");
-         scanf("%s",o);
-         fprintf(f,"%s\t",o);
-         printf("\n\n");
-         gotoxy(46,6);
-         printf("Enter Destination = ");
-         scanf("%s",d);
-         fprintf(f,"%s\t",d);
-         printf("\n\n");
-         printf("Enter Date Of Journey = ");
-         scanf("%d",&date);
-         fprintf(f,"%d",date);
-         printf("Enter Month Of Journey = ");
-         scanf("%s",month);
-         fprintf(f,"%s",month);
-         printf("Enter Year Of Journey = ");
-         scanf("%d",&year);
-         fprintf(f,"%d\t",year);
-         printf("\n");
-         printf("    Number Of Adults = ");
-         scanf("%d",&adult);
-         fprintf(f,"%d\t",adult);
-         printf("\n");
-         printf("    Number Of Childs = ");
-         scanf("%d",&child);
-         fprintf(f,"%d\t",child);
-         printf("\n");
-         printf("    Number Of Infants = ");
-         scanf("%d",&infant);
-         fprintf(f,"%d\t",infant);
-         printf("\n");
-         fclose(f);
-	 printf("Do u Want Flight Meal for +900 (yes/no)? = ");
-         scanf("%s",meal);
 	 clrscr();
+	 gotoxy(22,2);
+	 printf("SRM Welcomes You to Flight Booking Gateway\n\n");
+	 printf("--------------------------------------------------------------------------------\n");
+	 f=fopen("flight.txt","a");
+	 fprintf(f,"\n");
+	 printf("Enter Origin = ");
+	 scanf("%s",o);
+	 fprintf(f,"%s\t",o);
+	 printf("\n\n");
+	 gotoxy(46,6);
+	 printf("Enter Destination = ");
+	 scanf("%s",d);
+	 fprintf(f,"%s\t",d);
+	 printf("\n\n");
+	 printf("Enter Date Of Journey = ");
+	 scanf("%d",&date);
+	 fprintf(f,"%d",date);
+	 printf("Enter Month Of Journey = ");
+	 scanf("%s",month);
+	 fprintf(f,"%s",month);
+	 printf("Enter Year Of Journey = ");
+	 scanf("%d",&year);
+	 fprintf(f,"%d\t",year);
+	 printf("\n");
+	 printf("    Number Of Adults = ");
+	 scanf("%d",&adult);
+	 fprintf(f,"%d\t",adult);
+	 printf("\n");
+	 printf("    Number Of Childs = ");
+	 scanf("%d",&child);
+	 fprintf(f,"%d\t",child);
+	 printf("\n");
+	 printf("    Number Of Infants = ");
+	 scanf("%d",&infant);
+	 fprintf(f,"%d\t",infant);
+	 printf("\n");
+	 fclose(f);
+	 int total=adult+child+infant;
+	 printf("Do u Want Flight Meal for +900 (yes/no)? = ");
+	 scanf("%s",meal);
+	 clrscr();
+	 printf("Passanger Details : - \n");
+	 printf("--------------------------------------------------------------------------------\n");
+	 printf("Number of Total Traveler = %d\n\n",total);
+      /*	 struct passanger q;
+	 for(int i=0;i<total;i++)
+	 {
+	    printf("Enter Name of Traveler = ");
+	    scanf("%s",q[i].name);
+	    printf("Enter Age = ");
+	    scanf("%d",&q[i].age);
+	 } */
        }
        if(c==2)
        {
